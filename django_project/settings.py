0c2b3d586e9956a32fcebaa6e22819e5039ce4d6
Vimal Prasad V T <vimal.vt.prasad@gmail.com>
2022-11-09 15:33:43 +0530

    added enviroment var

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environs import Env
+
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fff7c0v0r!b886_c1npb(a=al&d6-*arajddd-!e%8d(2nadi('
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -91,6 +95,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': 5432,
     }
+    # "default": env.dj_db_url("DATABASE_URL",default="postgres://postgres@db/postgres")
 }
 
 
