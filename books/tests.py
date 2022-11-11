86a8d49008619ba05ee6110be2e67b1199ee73da
Vimal Prasad V T <vimal.vt.prasad@gmail.com>
2022-11-11 12:04:45 +0530

    added special permissions

--- books/tests.py
+++ books/tests.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.contrib.auth.models import Permission
+from django.test import TestCase,Client
 from django.urls import reverse
 
 
@@ -16,6 +17,9 @@ class BookTests(TestCase):
             email = "reviewuser@email.com",
             password = "testing321",
         )
+        cls.special_permission = Permission.objects.get(
+            codename="special_status"
+        ) 
         cls.book = Book.objects.create(
             title = "Harry Potter", 
             author = "JK Rowling", 
@@ -32,13 +36,27 @@ class BookTests(TestCase):
         self.assertEqual(f"{self.book.author}", "JK Rowling")
         self.assertEqual(f"{self.book.price}", "25.00")
     
-    def test_book_listview(self):
+
+    def test_book_listview_for_loggedin_users(self):
+        self.client.login(email= "reviewuser@email.com", password = "testing321")
         response = self.client.get(reverse("book_list"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Harry Potter")
         self.assertTemplateUsed(response, "books/book_list.html")
+
     
-    def test_book_detail_view(self):
+    def test_book_listview_for_logged_out_users(self):
+        self.client.logout()
+        response = self.client.get(reverse("book_list"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "%s?next=/books/" % (reverse("account_login")))
+        response = self.client.get("%s?next=/books/" % (reverse("account_login")))
+        self.assertContains(response, "LogIn")
+
+
+    def test_book_detail_view_with_permissions(self):
+        self.client.login(email= "reviewuser@email.com", password = "testing321")
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get("/books/1233/")
         self.assertEqual(response.status_code, 200)
@@ -46,3 +64,19 @@ class BookTests(TestCase):
         self.assertContains(response, "Harry Potter")
         self.assertContains(response, "an excellent review") # testing if review exist
         self.assertTemplateUsed(response, "books/book_detail.html")
+
+
+    # def test_book_listview(self):
+    #     response = self.client.get(reverse("book_list"))
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertContains(response, "Harry Potter")
+    #     self.assertTemplateUsed(response, "books/book_list.html")
+    
+    # def test_book_detail_view(self):
+    #     response = self.client.get(self.book.get_absolute_url())
+    #     no_response = self.client.get("/books/1233/")
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(no_response.status_code, 404)
+    #     self.assertContains(response, "Harry Potter")
+    #     self.assertContains(response, "an excellent review") # testing if review exist
+    #     self.assertTemplateUsed(response, "books/book_detail.html")
--- books/views.py
+++ books/views.py
@@ -1,15 +1,20 @@
 from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 
 from .models import Book
 # Create your views here.
 
-class BooksListView(ListView):
+class BooksListView(LoginRequiredMixin,ListView):
     model = Book
     context_object_name = 'book_list'
     template_name = "books/book_list.html"
+    login_url = "account_login" # redirect to login page if user not logged in
 
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin , DetailView):
+    '''If the user is logged in and is an author he is granted permission to view all books using the PermissionRequiredMixin'''
     model = Book
     template_name = "books/book_detail.html"
+    login_url = "account_login" # redirect to login page if user not logged in
+    permission_required = "books.special_status"
